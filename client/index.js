35a8075836fb9406d3a9e184893922e90ea529ae
Lidobix <pipoflutepouet@gmail.com>
2023-01-27 13:58:45 +0100

    ajout des preview + creation d'une fonction de build de page + modif schéma de db

--- client/index.js
+++ client/index.js
@@ -8,32 +8,32 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-console.log('JS lancé');
-const bouton = document.querySelector('button');
-const projectList = document.getElementById('projectList');
-fetch(`http://127.0.0.1:1234/`, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-})
-    .then((r) => {
-    return r.json();
-})
-    .then((r) => {
-    console.log(r);
-});
-const callProjects = () => __awaiter(void 0, void 0, void 0, function* () {
-    fetch(`http://127.0.0.1:1234/api`, {
+window.addEventListener('DOMContentLoaded', function () {
+    console.log('JS lancé');
+    // let onload: boolean = true;
+    const bouton = document.querySelector('button');
+    const body = document.querySelector('body');
+    const section = document.querySelector('section');
+    const storyTitle = 'Mon histoire';
+    const projectsTitle = 'Mes projets';
+    const contactTitle = 'Contact';
+    const projectList = document.getElementById('projectList');
+    console.log('lancement fetch');
+    ///////////// COUCOU SERVEUR /////////////////////////
+    fetch(`http://127.0.0.1:1234/`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
         },
     })
-        .then((result) => {
-        return result.json();
+        .then((r) => {
+        return r.json();
     })
-        .then((formattedResult) => {
+        .then((r) => {
+        console.log(r);
+    });
+    ///////////////////////////////////////////
+    function buildPage(formattedResult) {
         const allProjects = formattedResult;
         allProjects.forEach((project) => {
             var _a, _b;
@@ -47,6 +47,10 @@ const callProjects = () => __awaiter(void 0, void 0, void 0, function* () {
             const description = document.createElement('p');
             description.innerText = `${project.description}`;
             container.appendChild(description);
+            const view = document.createElement('img');
+            view.classList.add('view');
+            view.src = 'assets/screen.png';
+            container.appendChild(view);
             if ((_a = project.technos) === null || _a === void 0 ? void 0 : _a.length) {
                 const technoList = document.createElement('div');
                 (_b = project.technos) === null || _b === void 0 ? void 0 : _b.forEach((techno) => {
@@ -54,12 +58,54 @@ const callProjects = () => __awaiter(void 0, void 0, void 0, function* () {
                     logo.classList.add('logoTechno');
                     logo.src = `assets/images/${techno.toLowerCase()}.png`;
                     technoList.appendChild(logo);
-                    console.log(techno);
                 });
                 container.appendChild(technoList);
             }
             projectList === null || projectList === void 0 ? void 0 : projectList.appendChild(container);
         });
+    }
+    const callProjects = () => __awaiter(this, void 0, void 0, function* () {
+        fetch(`http://127.0.0.1:1234/api`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((result) => {
+            return result.json();
+        })
+            .then((formattedResult) => {
+            buildPage(formattedResult);
+            // const allProjects: Project[] = formattedResult;
+            // allProjects.forEach((project) => {
+            //   const container: HTMLDivElement = document.createElement('div');
+            //   const title: HTMLHeadElement = document.createElement('h3');
+            //   title.innerText = `${project.title}`;
+            //   container.appendChild(title);
+            //   const status: HTMLParagraphElement = document.createElement('p');
+            //   status.innerText = `${project.type} - ${project.status}`;
+            //   container.appendChild(status);
+            //   const description: HTMLParagraphElement = document.createElement('p');
+            //   description.innerText = `${project.description}`;
+            //   container.appendChild(description);
+            //   const view: HTMLImageElement = document.createElement('img');
+            //   view.classList.add('view');
+            //   view.src = 'assets/screen.png';
+            //   container.appendChild(view);
+            //   if (project.technos?.length) {
+            //     const technoList: HTMLDivElement = document.createElement('div');
+            //     project.technos?.forEach((techno) => {
+            //       const logo: HTMLImageElement = document.createElement('img');
+            //       logo.classList.add('logoTechno');
+            //       logo.src = `assets/images/${techno.toLowerCase()}.png`;
+            //       technoList.appendChild(logo);
+            //     });
+            //     container.appendChild(technoList);
+            //   }
+            //   projectList?.appendChild(container);
+            // });
+        });
     });
+    // callSiteElements();
+    callProjects();
 });
-bouton === null || bouton === void 0 ? void 0 : bouton.addEventListener('click', callProjects);
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,83 +1,136 @@
-console.log('JS lancé');
-const bouton: HTMLButtonElement | null = document.querySelector('button');
-const projectList: HTMLElement | null = document.getElementById('projectList');
+window.addEventListener('DOMContentLoaded', function () {
+  console.log('JS lancé');
+  // let onload: boolean = true;
+  const bouton: HTMLButtonElement | null = document.querySelector('button');
+  const body: HTMLBodyElement | null = document.querySelector('body');
+  const section: HTMLElement | null = document.querySelector('section');
+  const storyTitle: string = 'Mon histoire';
+  const projectsTitle: string = 'Mes projets';
+  const contactTitle: string = 'Contact';
 
-type Type = 'Projet perso' | "projet d'études";
-type Status = 'Fini' | 'En cours' | 'Stand-By';
-type Techno =
-  | 'HTML'
-  | 'CSS'
-  | 'JavaScript'
-  | 'TypeScript'
-  | 'WebSocket'
-  | 'Angular'
-  | 'React'
-  | 'NODE_JS';
+  const projectList: HTMLElement | null =
+    document.getElementById('projectList');
 
-type Project = {
-  title: string;
-  type: Type;
-  status: Status;
-  description: string;
-  technos?: Techno[];
-  image?: string;
-  link?: string;
-};
+  type Subtype = 'Projet perso' | "projet d'études";
+  type Type = 'project' | 'story';
+  type Status = 'Fini' | 'En cours' | 'Stand-By';
+  type Techno =
+    | 'HTML'
+    | 'CSS'
+    | 'JavaScript'
+    | 'TypeScript'
+    | 'WebSocket'
+    | 'Angular'
+    | 'React'
+    | 'NODE_JS';
 
-fetch(`http://127.0.0.1:1234/`, {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-})
-  .then((r) => {
-    return r.json();
-  })
-  .then((r) => {
-    console.log(r);
-  });
-const callProjects = async (): Promise<void> => {
-  fetch(`http://127.0.0.1:1234/api`, {
+  type Project = {
+    title: string;
+    type: Type;
+    subtype: Subtype;
+    status: Status;
+    description: string;
+    technos?: Techno[];
+    image?: string;
+    link?: string;
+  };
+  console.log('lancement fetch');
+
+  ///////////// COUCOU SERVEUR /////////////////////////
+  fetch(`http://127.0.0.1:1234/`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
   })
-    .then((result) => {
-      return result.json();
+    .then((r) => {
+      return r.json();
     })
-    .then((formattedResult) => {
-      const allProjects: Project[] = formattedResult;
-      allProjects.forEach((project) => {
-        const container: HTMLDivElement = document.createElement('div');
-
-        const title: HTMLHeadElement = document.createElement('h3');
-        title.innerText = `${project.title}`;
-        container.appendChild(title);
-
-        const status: HTMLParagraphElement = document.createElement('p');
-        status.innerText = `${project.type} - ${project.status}`;
-        container.appendChild(status);
-
-        const description: HTMLParagraphElement = document.createElement('p');
-        description.innerText = `${project.description}`;
-        container.appendChild(description);
-
-        if (project.technos?.length) {
-          const technoList: HTMLDivElement = document.createElement('div');
-          project.technos?.forEach((techno) => {
-            const logo: HTMLImageElement = document.createElement('img');
-            logo.classList.add('logoTechno');
-            logo.src = `assets/images/${techno.toLowerCase()}.png`;
-            technoList.appendChild(logo);
-            console.log(techno);
-          });
-          container.appendChild(technoList);
-        }
-
-        projectList?.appendChild(container);
-      });
+    .then((r) => {
+      console.log(r);
     });
-};
+  ///////////////////////////////////////////
 
-bouton?.addEventListener('click', callProjects);
+  function buildPage(formattedResult: Project[]): void {
+    const allProjects: Project[] = formattedResult;
+    allProjects.forEach((project) => {
+      const container: HTMLDivElement = document.createElement('div');
+
+      const title: HTMLHeadElement = document.createElement('h3');
+      title.innerText = `${project.title}`;
+      container.appendChild(title);
+
+      const status: HTMLParagraphElement = document.createElement('p');
+      status.innerText = `${project.type} - ${project.status}`;
+      container.appendChild(status);
+
+      const description: HTMLParagraphElement = document.createElement('p');
+      description.innerText = `${project.description}`;
+      container.appendChild(description);
+
+      const view: HTMLImageElement = document.createElement('img');
+      view.classList.add('view');
+      view.src = 'assets/screen.png';
+      container.appendChild(view);
+
+      if (project.technos?.length) {
+        const technoList: HTMLDivElement = document.createElement('div');
+        project.technos?.forEach((techno) => {
+          const logo: HTMLImageElement = document.createElement('img');
+          logo.classList.add('logoTechno');
+          logo.src = `assets/images/${techno.toLowerCase()}.png`;
+          technoList.appendChild(logo);
+        });
+        container.appendChild(technoList);
+      }
+
+      projectList?.appendChild(container);
+    });
+  }
+
+  const callProjects = async (): Promise<void> => {
+    fetch(`http://127.0.0.1:1234/api`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((result) => {
+        return result.json();
+      })
+      .then((formattedResult) => {
+        buildPage(formattedResult);
+        // const allProjects: Project[] = formattedResult;
+        // allProjects.forEach((project) => {
+        //   const container: HTMLDivElement = document.createElement('div');
+        //   const title: HTMLHeadElement = document.createElement('h3');
+        //   title.innerText = `${project.title}`;
+        //   container.appendChild(title);
+        //   const status: HTMLParagraphElement = document.createElement('p');
+        //   status.innerText = `${project.type} - ${project.status}`;
+        //   container.appendChild(status);
+        //   const description: HTMLParagraphElement = document.createElement('p');
+        //   description.innerText = `${project.description}`;
+        //   container.appendChild(description);
+        //   const view: HTMLImageElement = document.createElement('img');
+        //   view.classList.add('view');
+        //   view.src = 'assets/screen.png';
+        //   container.appendChild(view);
+        //   if (project.technos?.length) {
+        //     const technoList: HTMLDivElement = document.createElement('div');
+        //     project.technos?.forEach((techno) => {
+        //       const logo: HTMLImageElement = document.createElement('img');
+        //       logo.classList.add('logoTechno');
+        //       logo.src = `assets/images/${techno.toLowerCase()}.png`;
+        //       technoList.appendChild(logo);
+        //     });
+        //     container.appendChild(technoList);
+        //   }
+        //   projectList?.appendChild(container);
+        // });
+      });
+  };
+
+  // callSiteElements();
+  callProjects();
+});
