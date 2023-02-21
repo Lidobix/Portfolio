2932d69799ee99efc1505c3ddb462f602db61609
Lidobix <pipoflutepouet@gmail.com>
2023-02-21 22:28:24 +0100

    conversion du site en objet

--- client/index.js
+++ client/index.js
@@ -9,159 +9,155 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 window.addEventListener('DOMContentLoaded', function () {
-    console.log('JS lancé');
-    const script = document.querySelector('script');
-    const body = document.querySelector('body');
-    const navElements = [];
-    console.log('lancement fetch');
-    ///////////// COUCOU SERVEUR /////////////////////////
-    fetch(`http://127.0.0.1:1234/`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
+    const site = {
+        objet: this,
+        body: this.document.querySelector('body'),
+        script: this.document.querySelector('script'),
+        siteElements: {},
+        buildSite: function () {
+            var _a, _b, _c;
+            return __awaiter(this, void 0, void 0, function* () {
+                yield this.fetchElements();
+                console.log(this.siteElements);
+                const headerWrapper = document.createElement('div');
+                headerWrapper.classList.add();
+                headerWrapper.appendChild(this.buildHeader(this.siteElements.header));
+                (_a = this.body) === null || _a === void 0 ? void 0 : _a.insertBefore(headerWrapper, this.script);
+                (_b = this.body) === null || _b === void 0 ? void 0 : _b.insertBefore(this.buildSection(this, this.siteElements.section), this.script);
+                (_c = this.body) === null || _c === void 0 ? void 0 : _c.insertBefore(this.buildNav(this.siteElements.nav), document.querySelector('section'));
+            });
         },
-    })
-        .then((r) => {
-        return r.json();
-    })
-        .then((r) => {
-        console.log(r);
-    });
-    ///////////////////////////////////////////
-    function buildPage(siteElements) {
-        body === null || body === void 0 ? void 0 : body.insertBefore(buildHeader(siteElements.header), script);
-        body === null || body === void 0 ? void 0 : body.insertBefore(buildSection(siteElements.section), script);
-        body === null || body === void 0 ? void 0 : body.insertBefore(buildNav(navElements), document.querySelector('section'));
-    }
-    function buildForm(htmlForm) {
-        const form = document.createElement('form');
-        form.classList.add('card');
-        form.innerHTML = htmlForm;
-        form.method = 'POST';
-        return form;
-    }
-    function buildHeader(headerElements) {
-        const header = document.createElement('header');
-        const pageTitle = document.createElement('h1');
-        pageTitle.innerText = headerElements.title;
-        const subTitle = document.createElement('h2');
-        subTitle.innerText = headerElements.subtitle;
-        header.appendChild(pageTitle);
-        header.appendChild(subTitle);
-        return header;
-    }
-    function buildNav(navElements) {
-        const nav = document.createElement('nav');
-        const ul = document.createElement('ul');
-        navElements.forEach((element) => {
-            const li = document.createElement('li');
-            const a = document.createElement('a');
-            a.href = element.anchor;
-            a.innerText = element.name;
-            li.appendChild(a);
-            ul.appendChild(li);
-        });
-        const div = document.createElement('div');
-        for (let i = 0; i < 3; i++) {
-            const bullet = document.createElement('div');
-            div.appendChild(bullet);
-        }
-        nav.appendChild(div);
-        // nav.appendChild(ul);
-        return nav;
-    }
-    function buildSection(sectionElements) {
-        const section = document.createElement('section');
-        sectionElements.forEach((sectionElement) => {
-            if (sectionElement.display) {
-                const title = document.createElement('h3');
-                title.innerText = sectionElement.name;
-                const anchorCalc = sectionElement.name
-                    .toLowerCase()
-                    .split(' ')
-                    .sort((a, b) => b.length - a.length)[0];
-                title.id = anchorCalc;
-                navElements.push({
-                    name: sectionElement.name,
-                    anchor: `#${anchorCalc}`,
-                });
-                section.appendChild(title);
-                if (sectionElement.text) {
-                    const content = document.createElement('p');
-                    content.innerText = sectionElement.text;
-                    section.appendChild(content);
-                }
-                if (sectionElement.projectList) {
-                    section.appendChild(buildProjects(sectionElement.projectList));
-                }
-                if (sectionElement.htmlForm) {
-                    section.appendChild(buildForm(sectionElement.htmlForm));
-                }
-            }
-        });
-        return section;
-    }
-    function buildProjects(projects) {
-        const container = document.createElement('div');
-        container.id = 'projectList';
-        projects.forEach((project) => {
-            var _a, _b;
-            if (project.display) {
-                const card = document.createElement('div');
-                card.classList.add('card');
-                const titleContainer = document.createElement('div');
-                titleContainer.classList.add('projectTitleContainer');
-                const projectTitle = document.createElement('h4');
-                projectTitle.innerText = `${project.title}`;
-                titleContainer.appendChild(projectTitle);
-                card.appendChild(titleContainer);
-                const quickDescription = document.createElement('div');
-                quickDescription.classList.add('quickDescription');
-                const type = document.createElement('p');
-                type.innerText = `${project.type}`;
-                quickDescription.appendChild(type);
-                const status = document.createElement('p');
-                status.innerText = `${project.subtype} (${project.status})`;
-                quickDescription.appendChild(status);
-                card.appendChild(quickDescription);
-                if (project.image) {
-                    const view = document.createElement('img');
-                    view.classList.add('projectView');
-                    view.src = project.image;
-                    card.appendChild(view);
-                }
-                if ((_a = project.technos) === null || _a === void 0 ? void 0 : _a.length) {
-                    const technoList = document.createElement('div');
-                    technoList.classList.add('technoList');
-                    (_b = project.technos) === null || _b === void 0 ? void 0 : _b.forEach((techno) => {
-                        const div = document.createElement('div');
-                        const logo = document.createElement('img');
-                        div.classList.add('logoTechno');
-                        logo.src = `assets/images/${techno.toLowerCase()}.png`;
-                        div.appendChild(logo);
-                        technoList.appendChild(div);
+        buildHeader: (headerElements) => {
+            const header = document.createElement('header');
+            const pageTitle = document.createElement('h1');
+            pageTitle.innerText = headerElements.title;
+            const subTitle = document.createElement('h2');
+            subTitle.innerText = headerElements.subtitle;
+            header.appendChild(pageTitle);
+            header.appendChild(subTitle);
+            return header;
+        },
+        buildSection: (levelUp, sectionElements) => {
+            const section = document.createElement('section');
+            sectionElements.forEach((sectionElement) => {
+                if (sectionElement.display) {
+                    const title = document.createElement('h3');
+                    title.innerText = sectionElement.name;
+                    const anchorCalc = sectionElement.name
+                        .toLowerCase()
+                        .split(' ')
+                        .sort((a, b) => b.length - a.length)[0];
+                    title.id = anchorCalc;
+                    levelUp.siteElements.nav.push({
+                        name: sectionElement.name,
+                        anchor: `#${anchorCalc}`,
                     });
-                    card.appendChild(technoList);
+                    section.appendChild(title);
+                    if (sectionElement.text) {
+                        const content = document.createElement('p');
+                        content.innerText = sectionElement.text;
+                        section.appendChild(content);
+                    }
+                    if (sectionElement.projectList) {
+                        section.appendChild(levelUp.buildProjects(sectionElement.projectList));
+                    }
+                    if (sectionElement.htmlForm) {
+                        section.appendChild(levelUp.buildForm(sectionElement.htmlForm));
+                    }
                 }
-                container.appendChild(card);
+            });
+            return section;
+        },
+        buildNav: (navElements) => {
+            const nav = document.createElement('nav');
+            const ul = document.createElement('ul');
+            navElements.forEach((element) => {
+                const li = document.createElement('li');
+                const a = document.createElement('a');
+                a.href = element.anchor;
+                a.innerText = element.name;
+                li.appendChild(a);
+                ul.appendChild(li);
+                nav.appendChild(ul);
+            });
+            const div = document.createElement('div');
+            for (let i = 0; i < 3; i++) {
+                const bullet = document.createElement('div');
+                div.appendChild(bullet);
             }
-        });
-        return container;
-    }
-    const callSiteContent = () => __awaiter(this, void 0, void 0, function* () {
-        console.log('fetch datas');
-        fetch(`http://127.0.0.1:1234/api`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-            .then((result) => {
-            return result.json();
-        })
-            .then((datas) => {
-            buildPage(datas[0]);
-        });
-    });
-    callSiteContent();
+            nav.appendChild(div);
+            // nav.appendChild(ul);
+            return nav;
+        },
+        buildForm: (htmlForm) => {
+            const form = document.createElement('form');
+            form.classList.add('card');
+            form.innerHTML = htmlForm;
+            form.method = 'POST';
+            return form;
+        },
+        buildProjects: (projects) => {
+            const container = document.createElement('div');
+            container.id = 'projectList';
+            projects.forEach((project) => {
+                var _a, _b;
+                if (project.display) {
+                    const card = document.createElement('div');
+                    card.classList.add('card');
+                    const titleContainer = document.createElement('div');
+                    titleContainer.classList.add('projectTitleContainer');
+                    const projectTitle = document.createElement('h4');
+                    projectTitle.innerText = `${project.title}`;
+                    titleContainer.appendChild(projectTitle);
+                    card.appendChild(titleContainer);
+                    const quickDescription = document.createElement('div');
+                    quickDescription.classList.add('quickDescription');
+                    const type = document.createElement('p');
+                    type.innerText = `${project.type}`;
+                    quickDescription.appendChild(type);
+                    const status = document.createElement('p');
+                    status.innerText = `${project.subtype} (${project.status})`;
+                    quickDescription.appendChild(status);
+                    card.appendChild(quickDescription);
+                    if (project.image) {
+                        const view = document.createElement('img');
+                        view.classList.add('projectView');
+                        view.src = project.image;
+                        card.appendChild(view);
+                    }
+                    if ((_a = project.technos) === null || _a === void 0 ? void 0 : _a.length) {
+                        const technoList = document.createElement('div');
+                        technoList.classList.add('technoList');
+                        (_b = project.technos) === null || _b === void 0 ? void 0 : _b.forEach((techno) => {
+                            const div = document.createElement('div');
+                            const logo = document.createElement('img');
+                            div.classList.add('logoTechno');
+                            logo.src = `assets/images/${techno.toLowerCase()}.png`;
+                            div.appendChild(logo);
+                            technoList.appendChild(div);
+                        });
+                        card.appendChild(technoList);
+                    }
+                    container.appendChild(card);
+                }
+            });
+            return container;
+        },
+        fetchElements: () => {
+            return fetch(`http://127.0.0.1:1234/api`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+                .then((result) => {
+                return result.json();
+            })
+                .then((datas) => {
+                site.siteElements = datas[0];
+                return datas[0];
+            });
+        },
+    };
+    site.buildSite();
 });
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,4 @@
 window.addEventListener('DOMContentLoaded', function () {
-  console.log('JS lancé');
-
-  const script: HTMLScriptElement | null = document.querySelector('script');
-
-  const body: HTMLBodyElement | null = document.querySelector('body');
-
   type ProjectSubtype = 'Projet perso' | "Projet d'études" | 'Projet pro';
 
   type Header = {
@@ -54,189 +48,207 @@ window.addEventListener('DOMContentLoaded', function () {
     display: boolean;
   };
 
-  const navElements: NavElement[] = [];
-
-  console.log('lancement fetch');
-
-  ///////////// COUCOU SERVEUR /////////////////////////
-  fetch(`http://127.0.0.1:1234/`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((r) => {
-      return r.json();
-    })
-    .then((r) => {
-      console.log(r);
-    });
-  ///////////////////////////////////////////
-
-  function buildPage(siteElements: SiteElements): void {
-    body?.insertBefore(buildHeader(siteElements.header), script);
-    body?.insertBefore(buildSection(siteElements.section), script);
-    body?.insertBefore(
-      buildNav(navElements),
-      document.querySelector('section')
-    );
-  }
-
-  function buildForm(htmlForm: string): HTMLElement {
-    const form: HTMLFormElement = document.createElement('form');
-    form.classList.add('card');
-    form.innerHTML = htmlForm;
-    form.method = 'POST';
-
-    return form;
-  }
-
-  function buildHeader(headerElements: Header): HTMLElement {
-    const header: HTMLElement = document.createElement('header');
-
-    const pageTitle: HTMLHeadingElement = document.createElement('h1');
-    pageTitle.innerText = headerElements.title;
-    const subTitle: HTMLHeadingElement = document.createElement('h2');
-    subTitle.innerText = headerElements.subtitle;
-
-    header.appendChild(pageTitle);
-    header.appendChild(subTitle);
-
-    return header;
-  }
-
-  function buildNav(navElements: NavElement[]): HTMLElement {
-    const nav: HTMLElement = document.createElement('nav');
-    const ul: HTMLUListElement = document.createElement('ul');
-
-    navElements.forEach((element) => {
-      const li: HTMLLIElement = document.createElement('li');
-      const a: HTMLAnchorElement = document.createElement('a');
-      a.href = element.anchor;
-      a.innerText = element.name;
-      li.appendChild(a);
-      ul.appendChild(li);
-    });
-
-    const div: HTMLElement = document.createElement('div');
-
-    for (let i = 0; i < 3; i++) {
-      const bullet: HTMLElement = document.createElement('div');
-      div.appendChild(bullet);
-    }
-    nav.appendChild(div);
-    // nav.appendChild(ul);
-
-    return nav;
-  }
-
-  function buildSection(sectionElements: SectionElement[]): HTMLElement {
-    const section: HTMLElement = document.createElement('section');
-
-    sectionElements.forEach((sectionElement) => {
-      if (sectionElement.display) {
-        const title: HTMLHeadingElement = document.createElement('h3');
-        title.innerText = sectionElement.name;
-
-        const anchorCalc: string = sectionElement.name
-          .toLowerCase()
-          .split(' ')
-          .sort((a, b) => b.length - a.length)[0];
-        title.id = anchorCalc;
-        navElements.push({
-          name: sectionElement.name,
-          anchor: `#${anchorCalc}`,
-        });
-        section.appendChild(title);
-
-        if (sectionElement.text) {
-          const content: HTMLParagraphElement = document.createElement('p');
-          content.innerText = sectionElement.text;
-          section.appendChild(content);
-        }
-
-        if (sectionElement.projectList) {
-          section.appendChild(buildProjects(sectionElement.projectList));
-        }
-        if (sectionElement.htmlForm) {
-          section.appendChild(buildForm(sectionElement.htmlForm));
-        }
-      }
-    });
-
-    return section;
-  }
-
-  function buildProjects(projects: Project[]): HTMLElement {
-    const container: HTMLDivElement = document.createElement('div');
-    container.id = 'projectList';
-    projects.forEach((project) => {
-      if (project.display) {
-        const card: HTMLDivElement = document.createElement('div');
-        card.classList.add('card');
-
-        const titleContainer: HTMLElement = document.createElement('div');
-        titleContainer.classList.add('projectTitleContainer');
-        const projectTitle: HTMLHeadElement = document.createElement('h4');
-        projectTitle.innerText = `${project.title}`;
-        titleContainer.appendChild(projectTitle);
-        card.appendChild(titleContainer);
-
-        const quickDescription: HTMLDivElement = document.createElement('div');
-        quickDescription.classList.add('quickDescription');
-
-        const type: HTMLParagraphElement = document.createElement('p');
-        type.innerText = `${project.type}`;
-        quickDescription.appendChild(type);
-
-        const status: HTMLParagraphElement = document.createElement('p');
-        status.innerText = `${project.subtype} (${project.status})`;
-        quickDescription.appendChild(status);
-        card.appendChild(quickDescription);
-
-        if (project.image) {
-          const view: HTMLImageElement = document.createElement('img');
-          view.classList.add('projectView');
-          view.src = project.image;
-          card.appendChild(view);
-        }
-
-        if (project.technos?.length) {
-          const technoList: HTMLDivElement = document.createElement('div');
-          technoList.classList.add('technoList');
-          project.technos?.forEach((techno) => {
-            const div: HTMLElement = document.createElement('div');
-            const logo: HTMLImageElement = document.createElement('img');
-            div.classList.add('logoTechno');
-
-            logo.src = `assets/images/${techno.toLowerCase()}.png`;
-
-            div.appendChild(logo);
-            technoList.appendChild(div);
-          });
-          card.appendChild(technoList);
-        }
-        container.appendChild(card);
-      }
-    });
-    return container;
-  }
-
-  const callSiteContent = async (): Promise<void> => {
-    console.log('fetch datas');
-    fetch(`http://127.0.0.1:1234/api`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((result) => {
-        return result.json();
-      })
-      .then((datas) => {
-        buildPage(datas[0]);
-      });
+  type Site = {
+    objet: any;
+    body: HTMLElement | null;
+    script: HTMLScriptElement | null;
+    siteElements: SiteElements;
+    buildSite: Function;
+    fetchElements: Function;
+    buildHeader: Function;
+    buildSection: Function;
+    buildNav: Function;
+    buildForm: Function;
+    buildProjects: Function;
   };
 
-  callSiteContent();
+  const site: Site = {
+    objet: this,
+    body: this.document.querySelector('body'),
+    script: this.document.querySelector('script'),
+    siteElements: {} as SiteElements,
+
+    buildSite: async function (): Promise<void> {
+      await this.fetchElements();
+      console.log(this.siteElements);
+      const headerWrapper: HTMLDivElement = document.createElement('div');
+      headerWrapper.classList.add();
+      headerWrapper.appendChild(this.buildHeader(this.siteElements.header));
+      this.body?.insertBefore(headerWrapper, this.script);
+
+      this.body?.insertBefore(
+        this.buildSection(this, this.siteElements.section),
+        this.script
+      );
+
+      this.body?.insertBefore(
+        this.buildNav(this.siteElements.nav),
+        document.querySelector('section')
+      );
+    },
+
+    buildHeader: (headerElements: Header): HTMLElement => {
+      const header: HTMLElement = document.createElement('header');
+
+      const pageTitle: HTMLHeadingElement = document.createElement('h1');
+      pageTitle.innerText = headerElements.title;
+      const subTitle: HTMLHeadingElement = document.createElement('h2');
+      subTitle.innerText = headerElements.subtitle;
+
+      header.appendChild(pageTitle);
+      header.appendChild(subTitle);
+
+      return header;
+    },
+
+    buildSection: (
+      levelUp: Site,
+      sectionElements: SectionElement[]
+    ): HTMLElement => {
+      const section: HTMLElement = document.createElement('section');
+      sectionElements.forEach((sectionElement) => {
+        if (sectionElement.display) {
+          const title: HTMLHeadingElement = document.createElement('h3');
+          title.innerText = sectionElement.name;
+
+          const anchorCalc: string = sectionElement.name
+            .toLowerCase()
+            .split(' ')
+            .sort((a, b) => b.length - a.length)[0];
+          title.id = anchorCalc;
+
+          levelUp.siteElements.nav.push({
+            name: sectionElement.name,
+            anchor: `#${anchorCalc}`,
+          });
+          section.appendChild(title);
+
+          if (sectionElement.text) {
+            const content: HTMLParagraphElement = document.createElement('p');
+            content.innerText = sectionElement.text;
+            section.appendChild(content);
+          }
+
+          if (sectionElement.projectList) {
+            section.appendChild(
+              levelUp.buildProjects(sectionElement.projectList)
+            );
+          }
+
+          if (sectionElement.htmlForm) {
+            section.appendChild(levelUp.buildForm(sectionElement.htmlForm));
+          }
+        }
+      });
+
+      return section;
+    },
+
+    buildNav: (navElements: NavElement[]): HTMLElement => {
+      const nav: HTMLElement = document.createElement('nav');
+      const ul: HTMLUListElement = document.createElement('ul');
+
+      navElements.forEach((element) => {
+        const li: HTMLLIElement = document.createElement('li');
+        const a: HTMLAnchorElement = document.createElement('a');
+        a.href = element.anchor;
+        a.innerText = element.name;
+        li.appendChild(a);
+        ul.appendChild(li);
+        nav.appendChild(ul);
+      });
+
+      const div: HTMLElement = document.createElement('div');
+
+      for (let i = 0; i < 3; i++) {
+        const bullet: HTMLElement = document.createElement('div');
+        div.appendChild(bullet);
+      }
+      nav.appendChild(div);
+
+      return nav;
+    },
+
+    buildForm: (htmlForm: string): HTMLElement => {
+      const form: HTMLFormElement = document.createElement('form');
+      form.classList.add('card');
+      form.innerHTML = htmlForm;
+      form.method = 'POST';
+
+      return form;
+    },
+
+    buildProjects: (projects: Project[]): HTMLElement => {
+      const container: HTMLDivElement = document.createElement('div');
+      container.id = 'projectList';
+      projects.forEach((project) => {
+        if (project.display) {
+          const card: HTMLDivElement = document.createElement('div');
+          card.classList.add('card');
+
+          const titleContainer: HTMLElement = document.createElement('div');
+          titleContainer.classList.add('projectTitleContainer');
+          const projectTitle: HTMLHeadElement = document.createElement('h4');
+          projectTitle.innerText = `${project.title}`;
+          titleContainer.appendChild(projectTitle);
+          card.appendChild(titleContainer);
+
+          const quickDescription: HTMLDivElement =
+            document.createElement('div');
+          quickDescription.classList.add('quickDescription');
+
+          const type: HTMLParagraphElement = document.createElement('p');
+          type.innerText = `${project.type}`;
+          quickDescription.appendChild(type);
+
+          const status: HTMLParagraphElement = document.createElement('p');
+          status.innerText = `${project.subtype} (${project.status})`;
+          quickDescription.appendChild(status);
+          card.appendChild(quickDescription);
+
+          if (project.image) {
+            const view: HTMLImageElement = document.createElement('img');
+            view.classList.add('projectView');
+            view.src = project.image;
+            card.appendChild(view);
+          }
+
+          if (project.technos?.length) {
+            const technoList: HTMLDivElement = document.createElement('div');
+            technoList.classList.add('technoList');
+            project.technos?.forEach((techno) => {
+              const div: HTMLElement = document.createElement('div');
+              const logo: HTMLImageElement = document.createElement('img');
+              div.classList.add('logoTechno');
+
+              logo.src = `assets/images/${techno.toLowerCase()}.png`;
+
+              div.appendChild(logo);
+              technoList.appendChild(div);
+            });
+            card.appendChild(technoList);
+          }
+          container.appendChild(card);
+        }
+      });
+      return container;
+    },
+    fetchElements: (): Promise<SiteElements> => {
+      return fetch(`http://127.0.0.1:1234/api`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((result) => {
+          return result.json();
+        })
+        .then((datas) => {
+          site.siteElements = datas[0];
+          return datas[0];
+        });
+    },
+  };
+
+  site.buildSite();
 });
