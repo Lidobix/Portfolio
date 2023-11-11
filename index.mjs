ea23ca3e6a44c134fcaf10f77d9f6ac326123e1c
Lidobix <pipoflutepouet@gmail.com>
2023-11-11 01:00:57 +0100

    intégration loader

--- index.mjs
+++ index.mjs
@@ -2,12 +2,19 @@ import { EventsManager } from './src/events.mjs';
 import datasManager from './src/datasManager.mjs';
 import siteBuilder from './src/site.mjs';
 const eventsManager = new EventsManager();
+import loader from './src/loader.mjs';
 
 window.addEventListener('DOMContentLoaded', function () {
-  datasManager.fetchElements().then(() => {
-    siteBuilder.buildHeader();
-    siteBuilder.buildNav();
-    siteBuilder.buildSection();
-    eventsManager.addEvents();
-  });
+  loader.open();
+  datasManager
+    .fetchElements()
+    .then(() => {
+      siteBuilder.buildHeader();
+      siteBuilder.buildNav();
+      siteBuilder.buildSection();
+      eventsManager.addEvents();
+    })
+    .then(() => {
+      loader.close();
+    });
 });
