c26d936bf38afd9ad333b2b440721ad423e70b6d
Lidobix <pipoflutepouet@gmail.com>
2023-12-02 15:55:19 +0100

    retrait controle rotation tel

--- src/events.mjs
+++ src/events.mjs
@@ -13,9 +13,6 @@ class EventsManager {
     this.isNavDisplayed = false;
     this.navToggle;
     this.arrow;
-    this.currentScrollY;
-    this.verticalBodyHeight;
-    this.horizontalBodyHeight;
   }
 
   init() {
@@ -25,8 +22,7 @@ class EventsManager {
 
   scrollPage() {
     window.addEventListener('scroll', () => {
-      this.currentScrollY = window.scrollY;
-      if (this.currentScrollY > 200) {
+      if (window.scrollY > 200) {
         this.arrow.classList.remove('invisible');
         this.arrow.classList.add('visible');
       } else {
@@ -79,50 +75,12 @@ class EventsManager {
       Preview.updateImage(1);
     });
   }
+
   escapeKey() {
     document.addEventListener('keydown', (e) => {
       if (e.code === 'Escape' && this.isPreviewDisplayed) {
         Preview.close();
         this.isPreviewDisplayed = false;
-      }
-    });
-  }
-
-  rotatePhone() {
-    window.addEventListener('orientationchange', () => {
-      if (window.orientation === 0) {
-        // Appareil en position portrait
-
-        setTimeout(() => {
-          this.verticalBodyHeight = !!this.verticalBodyHeight
-            ? this.verticalBodyHeight
-            : parseFloat(window.getComputedStyle(this.body).height);
-
-          console.log('verticalBodyHeight', this.verticalBodyHeight);
-        }, 100);
-        // au lancement de l'app, déterminer la hauteur du body en fonction de si on est en portrait ou en paysage
-        // utiliser cette donnée pour faire le calcul du % de la hauteur à scroller.
-
-        // const equivVerticalScroll=this.currentScrollY/this.
-        // this.lastHorizontalScrollY = window.scrollY;
-        setTimeout(() => {
-          window.scroll(0, this.lastVerticalScrollY);
-        }, 100);
-      } else if (window.orientation === 90 || window.orientation === -90) {
-        // Appareil en position paysage
-
-        setTimeout(() => {
-          this.horizontalBodyHeight = !!this.horizontalBodyHeight
-            ? this.horizontalBodyHeight
-            : parseFloat(window.getComputedStyle(this.body).height);
-
-          console.log('horizontalBodyHeight', this.horizontalBodyHeight);
-        }, 100);
-
-        this.lastVerticalScrollY = window.scrollY;
-        setTimeout(() => {
-          window.scroll(0, this.lastHorizontalScrollY);
-        }, 100);
       }
     });
   }
@@ -190,6 +148,5 @@ export const addEvents = () => {
   eventManager.clicOnPage();
   eventManager.scrollPage();
   eventManager.escapeKey();
-  eventManager.rotatePhone();
   eventManager.submitForm();
 };
